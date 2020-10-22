4293a4fc9844d1488fc2f7258b9466c7516a4908
Sohiduzzaman Prince <prince@secureage.com>
2020-10-22 11:43:51 +0800

    Added large file creation script

--- largefile.sh
+++ largefile.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
-for n in {1..10000}; do
-    dd if=/dev/urandom of=file$( printf %03d "$n" ).bin bs=1 count=$(( RANDOM + 1024 ))
+output_dir="target"
+mkdir -p $output_dir
+for n in {1..10}; do
+    dd if=/dev/urandom of=$output_dir/file$n.bin bs=1 count=$(( RANDOM + 1024 ))
+    #dd if=/dev/zero of=file$.bin bs=1024 count=1
 done
